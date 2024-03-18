24846a2f490e722ebaae01d7fe9f1c43247b6b97
bobobo77 <36542594+bobobo77@users.noreply.github.com>
2024-03-17 22:24:52 -0400

    added a check to see if the user has apt and if they do, can update some things and install others

--- starterpack.sh
+++ starterpack.sh
@@ -2,7 +2,20 @@
 
 if [[ "$(id -u)" -eq 0 ]]; then
     echo "Script is running as root"
-
+	
+	echo $(which apt)
+	if [[ -n "$(which apt)"]]; then
+		echo "apt is installed properly"
+		apt install update     	# I do these by default
+		apt install upgrade		# I do this by default to ensure the linux ditro is up to date with commands
+		apt install -y \
+			dnsutils \
+			nmap \
+			
+	else 
+		echo "Unable to find apt"
+		exit
+	fi	
 else
     echo "You are not root." 1>&2
     exit
