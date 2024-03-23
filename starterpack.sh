c8d392c6e03876a8afcce43c7db26d3ccc7caa51
bobobo77 <36542594+bobobo77@users.noreply.github.com>
2024-03-22 23:21:33 -0400

    subshell to cd ~

--- starterpack.sh
+++ starterpack.sh
@@ -31,7 +31,7 @@ if [[ "$(id -u)" -eq 0 ]]; then
 		echo "Unable to find apt"
 		exit 1
 	fi
-	curl -o ~ https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh 
+	(cd ~; curl -o ~ https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh )
 		
 		bash ~/Anaconda3-2024.02-1-Linux-x86_64.sh
 		echo "downloaded Anaconda"
