68d096fdaa63175e2b11b06f47c407b5cc0fda0f
bobobo77 <36542594+bobobo77@users.noreply.github.com>
2025-02-21 14:55:42 -0500

    commented sources

--- vim-config/vundlebundle.sh
+++ vim-config/vundlebundle.sh
@@ -11,6 +11,11 @@
 
     cp .vimrc ~/.vimrc
     #copying the .vimrc from the repo file because it doesn't like to be made on default
-
+    #Sources:
+    #https://vimawesome.com/?p=2&q=tag%3Asyntax - suggestions for plugins
+    #https://github.com/nanotech/jellybeans.vim - how to run color schemes in .vimrc
+    #https://www.redhat.com/en/blog/five-vim-plugins - suggestions for plugins    
+    # ChatGPT - how to add a plugin from a github link that you can't find the command for
+    # https://github.com/cseelus/vim-colors-lucid - color scheme
     vim +PluginInstall +qall
     #this activates the .vimrc 
